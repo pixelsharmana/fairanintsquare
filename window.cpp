721501ddcb748f5f529b7fdfc1924cce911b29c1
doesthiscountasausernameplssayyes <ericgediman@gmail.com>
2019-04-01 15:40:02 -0700

    created new window code

--- window.cpp
+++ window.cpp
@@ -6,4 +6,11 @@ int main ( int argc, char *argv[] ) {
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_WM_SetCaption("Fair ain't square", "Fair ain't square");
     SDL_Surface* screen = SDL_SetVideoMode(640, 480, 0, 0);
+    SDL_Surface* temp = SDL_LoadBMP("img/menu.bmp");
+    if (temp == NULL) {
+        cout<<"No image found";
+    }
+    SDL_Surface* bg = SDL_DisplayFormat(temp);
+    SDL_FreeSurface(temp);
+    return 0;
 }
