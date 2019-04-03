626e87e98774f8407ccf6f25df06ef87f2b639c9
That Green Power Button <pixel_sharmana@hotmail.com>
2019-04-03 19:47:32 -0400

    add marginally better changes to physics class

--- physics.hpp
+++ physics.hpp
@@ -3,19 +3,20 @@
 
 class physics{
 private:
+  sf::Vector2f acceleration={};
+  sf::Vector2f velocity={};
   double accX=0.0;
   double accY=0.0;
   double velX=0.0;
   double velY=0.0;
 public:
   void changeAcceleration(const double x, const double y){
-	accX+=x;
-	accY+=y;
-	velX+=accX;
-	velY=accY;
+	acceleration.x=x;
+	acceleration.y=y;
+	velocity+=acceleration;
   }
   sf::Vector2f updateVelocity(){
-	return sf::Vector2f(velX, velY);
+	return velocity;
   }
 };
 
