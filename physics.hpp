7219239783df33efbaebf3bdc00ad1d976c9033b
That Green Power Button <pixel_sharmana@hotmail.com>
2019-04-03 21:11:25 -0400

    remove useless variables

--- physics.hpp
+++ physics.hpp
@@ -5,10 +5,6 @@ class physics{
 private:
   sf::Vector2f acceleration={};
   sf::Vector2f velocity={};
-  double accX=0.0;
-  double accY=0.0;
-  double velX=0.0;
-  double velY=0.0;
 public:
   void changeAcceleration(const double x, const double y){
 	acceleration.x=x;
